5fb0f8f0cd63ad7bd5b615dd076836ff2ac2cfa1
Donovan O'Linn <donovan.b.olinn@gmail.com>
2022-07-08 21:38:07 -0400

    Second Codewars

--- main.py
+++ main.py
@@ -22,3 +22,22 @@ def oddOrEven(arr):
 
 
 #------------------- 2 ----------------------------------
+
+'''
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1,2,3,10,5]) # should return [1,2,3,5,10]
+solution(None) # should return []
+'''
+def solution(nums):
+    return [] if nums == None else sorted(nums)
+
+#Better answer:
+def solution(nums):
+    return sorted(nums) if nums else []
+
+#------------------- 3 ----------------------------------
+
+
